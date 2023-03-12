9af1b2842db55b9fbdd8144b068577567c9e12d5
suprabathk <suprabathkondapally@gmail.com>
2023-03-12 16:02:29 +0530

    Added DB Functions

--- backend/models/task.js
+++ backend/models/task.js
@@ -9,6 +9,64 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static addTask({ title, startTime, endTime, userID }) {
+      return this.create({
+        title,
+        startTime,
+        endTime,
+        userID,
+      });
+    }
+
+    static removeTask({ id, userID }) {
+      return this.destroy({
+        where: {
+          id,
+          userID,
+        },
+      });
+    }
+
+    static getTasks(userID) {
+      return this.findAll({
+        where: {
+          userID,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static getTaskAtSlot({ startTime, endTime }) {
+      return this.findOne({
+        where: {
+          startTime,
+          endTime,
+        },
+      });
+    }
+
+    static getTask({ userID, id }) {
+      return this.findOne({
+        where: {
+          userID,
+          id,
+        },
+      });
+    }
+
+    static async updateTask({ title, id }) {
+      return this.update(
+        {
+          title,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+    }
+
     static associate(models) {
       Task.belongsTo(models.User, {
         foreignKey: "userID",
