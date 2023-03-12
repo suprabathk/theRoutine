42683a69555e41c98be6882b9d7c018cd4245cc3
suprabathk <suprabathkondapally@gmail.com>
2023-03-12 19:05:39 +0530

    Added slot reservation

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const { EmptyResultError } = require("sequelize");
 const router = express.Router()
 const { Task, User } = require("../models")
 
@@ -6,17 +7,54 @@ const userID = 1;
 
 router.get("/", async (req, res) => {
     const allTasks = await Task.getTasks(userID);
-    return res.json(allTasks)
+    return res.status(200).json(allTasks)
 })
 
 router.post("/", async (req, res) => {
-    const task = await Task.addTask({
-        title: req.body.title,
-        startTime: new Date(),
-        endTime: new Date(),
-        userID: userID
-    })
-    return res.json(task)
+    const startTime = new Date(req.body.startTime)
+    const endTime = new Date(req.body.endTime)
+
+    const allTasks = await Task.getTasks(userID);
+    const reservedSlots = []
+    for (let task of allTasks) {
+        reservedSlots.push([task.startTime, task.endTime])
+    }
+    empty = true
+    occupiedBy = null
+    for (let slotNo in reservedSlots) {
+        slot = reservedSlots[slotNo]
+        if (startTime > endTime) {
+            empty = false
+            occupiedBy = slotNo
+            break
+        }
+        if ((slot[0] <= startTime) && (startTime < slot[1])) {
+            empty = false
+            occupiedBy = slotNo
+            break
+        }
+        if ((slot[0] < endTime) && (endTime <= slot[1])) {
+            empty = false
+            occupiedBy = slotNo
+            break
+        }
+        if (startTime < slot[0] && slot[1] < endTime) {
+            empty = false
+            occupiedBy = slotNo
+            break
+        }
+    }
+    if (empty) {
+        const task = await Task.addTask({
+            title: req.body.title,
+            startTime: startTime,
+            endTime: endTime,
+            userID: userID
+        })
+        return res.status(200).json({ occupied: false, task: task })
+    } else {
+        return res.status(400).json({ occupied: true, occupiedBy: occupiedBy })
+    }
 })
 
 router.delete("/:id", async (req, res) => {
@@ -24,7 +62,7 @@ router.delete("/:id", async (req, res) => {
         id: req.params.id,
         userID: userID
     })
-    return res.json({ success: status === 1 })
+    return res.status(200).json({ success: status === 1 })
 })
 
 router.put("/:id", async (req, res) => {
@@ -33,7 +71,7 @@ router.put("/:id", async (req, res) => {
         id: req.params.id,
         userID: userID
     })
-    return res.json({ success: status === 1 })
+    return res.status(200).json({ success: status === 1 })
 })
 
 module.exports = router
