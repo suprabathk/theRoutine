29056baa54a01ed6fb6d7f3b32f65bca7f1d77ac
suprabathk <suprabathkondapally@gmail.com>
2023-03-14 20:11:58 +0530

    Added individual tasks to Users

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -5,11 +5,10 @@ const requireAuth = require("../middleware/requireAuth")
 const router = express.Router()
 
 router.use(requireAuth)
-const userID = 1;
 
 router.get("/", async (req, res) => {
     try {
-        const allTasks = await Task.getTasks(userID);
+        const allTasks = await Task.getTasks(req.user);
         return res.status(200).json(allTasks)
     } catch (error) {
         console.log(error);
@@ -19,7 +18,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        const task = await Task.getTask({ userID: userID, id: req.params.id })
+        const task = await Task.getTask({ userID: req.user, id: req.params.id })
         return res.json(task)
     } catch (error) {
         console.log(error);
@@ -35,7 +34,7 @@ router.post("/", async (req, res) => {
         const startTime = new Date(req.body.startTime)
         const endTime = new Date(req.body.endTime)
 
-        const allTasks = await Task.getTasks(userID);
+        const allTasks = await Task.getTasks(req.user);
         const reservedSlots = []
         for (let task of allTasks) {
             reservedSlots.push([task.startTime, task.endTime, task.id])
@@ -67,16 +66,16 @@ router.post("/", async (req, res) => {
                 title: req.body.title,
                 startTime: startTime,
                 endTime: endTime,
-                userID: userID
+                userID: req.user
             })
             return res.status(200).json({ occupied: false, task: task })
         } else {
-            occupiedByName = await Task.getTask({ userID: 1, id: occupiedBy })
+            occupiedByName = await Task.getTask({ userID: req.user, id: occupiedBy })
             return res.status(400).json({ occupied: true, occupiedBy: occupiedBy, occupiedByName: occupiedByName })
         }
     } catch (error) {
         console.log(error);
-        occupiedByName = await Task.getTask({ userID: 1, id: occupiedBy })
+        occupiedByName = await Task.getTask({ userID: req.user, id: occupiedBy })
         return res.status(400).json({ occupied: true, occupiedBy: occupiedBy, occupiedByName: occupiedByName })
     }
 })
@@ -85,7 +84,7 @@ router.delete("/:id", async (req, res) => {
     try {
         const status = await Task.removeTask({
             id: req.params.id,
-            userID: userID
+            userID: req.user
         })
         return res.status(200).json({ success: status === 1 })
     } catch (error) {
@@ -99,7 +98,7 @@ router.put("/:id", async (req, res) => {
         const status = await Task.updateTask({
             title: req.body.title,
             id: req.params.id,
-            userID: userID
+            userID: req.user
         })
         return res.status(200).json({ success: status === 1 })
     } catch (error) {
