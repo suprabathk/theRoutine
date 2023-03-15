1b6984559a31432bf8426ee91ab960d686ee471e
suprabathk <suprabathkondapally@gmail.com>
2023-03-15 15:55:13 +0530

    Added loading animation

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,20 @@
+import "./Loading.css"
+
+const LoadingComponent = () => {
+    return (
+        <div class="center">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+    );
+}
+
+export default LoadingComponent;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import Calender from "../components/Calender"
+import LoadingComponent from "../components/Loading"
 import useFetch from '../hooks/useFetch'
 
 
@@ -8,7 +9,7 @@ const HomePage = () => {
     return (
         <div className="app">
             {error && <div className="mx-20 my-2 w-full bg-purple-200 rounded-md px-2 py-1 text-purple-900 border border-purple-900 ">{error}</div>}
-            {isPending && !error && <div className="mx-80 my-4">Loading...</div>}
+            {isPending && !error && <LoadingComponent />}
             {meetings && <Calender meetings={meetings} />}
         </div>
     );
